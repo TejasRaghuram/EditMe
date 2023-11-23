d27ac1fe24b5379a60572116c1ca4d5c002c2ca9
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-23 18:04:10 -0500

    Changed HTML to Python Markdown

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from taipy import Gui
-from taipy.gui import Html
 
-index = Html("""
-    <h1>This is the home page</h1>
-""")
+index = """
+# This is the home page
+"""
 
-register = Html("""
-    <h1>This is the register page</h1>
-""")
+register = """
+# This is the register page
+"""
 
-login = Html("""
-    <h1>This is the login page</h1>
-""")
+login = """
+# This is the login page
+"""
 
 pages = {
     'index': index,
     'register': register,
     'login': login,
-}
+}   
 
 Gui(pages=pages, css_file="./styles.css").run(dark_mode=True)
