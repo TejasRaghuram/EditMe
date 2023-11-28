8a3c6246ccf2680f797d4b3fe0f88ae23fe5bdc8
Vignesh Saravanakumar <74271045+vigneshsaravanakumar404@users.noreply.github.com>
2023-11-27 21:50:50 -0500

    Once run code to create db

--- OnceRun.py
+++ OnceRun.py
@@ -1,1 +1,12 @@
+from MongoDBManager import MongoDBManager
 
+# Create a MongoDBManager instance
+mongo_manager = MongoDBManager(
+    collection_name="<COLLECTION_NAME>",
+    database_name="<DATABASE_NAME>"
+)
+
+
+initial_json_data = {}
+response = mongo_manager.write(initial_json_data)
+print(response) 
